a370c6e157975f06f53d20168918ec2504c5240d
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2023-12-28 02:07:43 +0300

    feedvc

--- Snapchat/ViewController/FeedViewController.swift
+++ Snapchat/ViewController/FeedViewController.swift
@@ -6,24 +6,54 @@
 //
 
 import UIKit
+import Firebase
 
-class FeedViewController: UIViewController {
-
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let fireStoreDatabase = Firestore.firestore()
+    var userNameArray = [String]()
+    var userEmailArray = [String]()
+    var documentIDArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        getUserInfo()
     }
     
+    func getUserInfo() {
+        fireStoreDatabase.collection("UserInfo").whereField("email", isEqualTo: Auth.auth().currentUser!.email!).getDocuments { snapShot, error in
+            if error != nil {
+                self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+            }else {
+                if snapShot?.isEmpty != true {
+                    for document in snapShot!.documents {
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+                        if let userName = document.get("username") as? String {
+                            UserSingleton.sharedUserInfo.userName = userName
+                        }
+                        if let userEmail = document.get("email") as? String {
+                            UserSingleton.sharedUserInfo.email = userEmail
+                        }
+                    }
+                }
+            }
+        }
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return userNameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = userNameArray[indexPath.row]
+        return cell
+    }
 }
+
